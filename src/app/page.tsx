a68a8a7cd1e4c932714820b377b9b92d7ea0e36c
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-02-08 07:32:07 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import { TextEffect } from '@/components/ui/Animations/text-effect';
-import { TextLoop } from '@/components/ui/Animations/text-loop';
-import { useState, useEffect } from 'react';
+import { TextEffect } from "@/components/ui/Animations/text-effect";
+import { TextLoop } from "@/components/ui/Animations/text-loop";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [showTextLoop, setShowTextLoop] = useState(false);
@@ -19,21 +19,23 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] bg-neutral-900 text-neutral-100 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <h1 className="font-bold text-9xl">
-          <TextEffect per='char' delay={0.5}>
+          <TextEffect per="char" delay={0.5}>
             Project Arcs
           </TextEffect>
         </h1>
 
         <div className="text-center font-light text-4xl space-y-4">
           <TextEffect delay={1.5}>
-          Nicknames management in your Discord server, but fun.
+            Nicknames management in your Discord server, but fun.
           </TextEffect>
-          
-          <div className={`transition-opacity font-semibold duration-1000 ${showTextLoop ? 'opacity-100' : 'opacity-0'}`}>
+
+          <div
+            className={`transition-opacity font-semibold duration-1000 ${showTextLoop ? "opacity-100" : "opacity-0"}`}
+          >
             <TextLoop
-              className='overflow-y-clip text-3xl'
+              className="overflow-y-clip text-3xl"
               transition={{
-                type: 'spring',
+                type: "spring",
                 stiffness: 900,
                 damping: 80,
                 mass: 10,
@@ -43,19 +45,19 @@ export default function Home() {
                   y: 20,
                   rotateX: 90,
                   opacity: 0,
-                  filter: 'blur(4px)',
+                  filter: "blur(4px)",
                 },
                 animate: {
                   y: 0,
                   rotateX: 0,
                   opacity: 1,
-                  filter: 'blur(0px)',
+                  filter: "blur(0px)",
                 },
                 exit: {
                   y: -20,
                   rotateX: -90,
                   opacity: 0,
-                  filter: 'blur(4px)',
+                  filter: "blur(4px)",
                 },
               }}
             >
@@ -69,14 +71,11 @@ export default function Home() {
               <span>League of Legends</span>
               <span>Severance</span>
             </TextLoop>
-            <h3 className='inline-flex whitespace-pre-wrap text-3xl'>
-              {' '} arc?
-            </h3>
+            <h3 className="inline-flex whitespace-pre-wrap text-3xl"> arc?</h3>
           </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
